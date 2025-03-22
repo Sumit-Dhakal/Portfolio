c6d5e07586ddf63a9985fce8354f5e6902532c3a
Sumit-Dhakal <125233468+Sumit-Dhakal@users.noreply.github.com>
2025-03-23 04:43:09 +0545

    Update index.jsx

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -80,7 +80,7 @@ const AboutDetails = () => {
             </Link>
 
             {/* Video inside Monster Shooter Repo */}
-            <div className="mt-4">
+            <div className="mt-4 h-auto">
                 <iframe
                 src="https://player.vimeo.com/video/1068487864?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;autoplay=1&amp;muted=1"
                 width="100%"
