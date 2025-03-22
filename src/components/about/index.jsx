ad1f49f83ce83fa67e621476d6979b188f6b9502
Sumit-Dhakal <125233468+Sumit-Dhakal@users.noreply.github.com>
2025-03-23 03:51:38 +0545

    Update index.jsx

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -80,7 +80,7 @@ const AboutDetails = () => {
   {/* Right Side: Monster Shooter Video */}
   <div className="flex-1">
     <video controls width="100%" height="315">
-      <source src="https://your-video-link.com/video.mp4" type="video/mp4" />
+      <source src="https://drive.google.com/file/d/1CwxlmaZJY1mvZ6Zw7-AJWeU13Ad6EhYR/view?usp=sharing" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   </div>
